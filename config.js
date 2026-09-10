4813fbb5f5a785f560066addcc7105cadaaf4a8f
BreakAI Automation <tokuume@breakai-labs.co.jp>
2026-09-10 09:59:47 +0900

    feat: activate live revenue funnel

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BREAKAI_CONFIG={
-  live:false,
-  freeIntakeUrl:"",
-  starterUrl:"",
-  reportUrl:""
+  live:true,
+  freeIntakeUrl:"https://buy.stripe.com/7sYbJ21jVeuOdKw8WH1gs00",
+  starterUrl:"https://buy.stripe.com/6oU4gA0fRcmGcGsb4P1gs01",
+  reportUrl:"https://buy.stripe.com/aFabJ20fR72mayka0L1gs04"
 };
