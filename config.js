f5c4f6ce53201dee42305334f8f33e29d2b91cad
BreakAI Automation <tokuume@breakai-labs.co.jp>
2026-09-11 09:30:22 +0900

    chore: hold paid offers during delivery QA

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.BREAKAI_CONFIG={
   live:true,
+  paidLive:false,
   freeIntakeUrl:"https://buy.stripe.com/7sYbJ21jVeuOdKw8WH1gs00",
   starterUrl:"https://buy.stripe.com/aFa14o2nZbiCbCo5Kv1gs02",
   reportUrl:"https://buy.stripe.com/5kQfZibYz9aucGs6Oz1gs03"
